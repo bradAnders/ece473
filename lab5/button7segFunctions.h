f0e17200ec707bcba31dfc7b42ad1812d4617e89
Brad <anderbra@oregonstate.edu>
2017-12-05 18:30:37 -0800

    starting atmega48 work

--- lab5/button7segFunctions.h
+++
@@ -1,4 +0,0 @@
-void digit_init();
-uint8_t chk_button(uint8_t button);
-void segsum(uint16_t sum);
-void toggle_button_bus();
--- lab5/encoderFunctions.h
+++
@@ -1,5 +0,0 @@
-void interpret_encoders();
-uint8_t checkDirection(uint8_t curr, uint8_t prev);
-void increment(uint8_t);
-void decrement(uint8_t);
-enum states; //{DISP_TIME, SET_TIME, ALARM, SNOOZE, SET_ALARM};
